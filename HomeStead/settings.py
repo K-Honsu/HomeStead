0db8536aed3d5d4f4a6abeda1e633b24bfcd7daf
K-Honsu <feranmia51@gmail.com>
2023-08-01 01:25:40 +0100

    change db settings file

--- HomeStead/settings.py
+++ HomeStead/settings.py
@@ -84,10 +84,10 @@ WSGI_APPLICATION = 'HomeStead.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER':config('DB_USER'),
-        'PASSWORD':config('DB_PASSWORD'),
-        'HOST':config('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASSWORD'),
+        'HOST':os.environ.get('DB_HOST'),
     }
 }
 database_url = os.environ.get('DATABASE_URL')
